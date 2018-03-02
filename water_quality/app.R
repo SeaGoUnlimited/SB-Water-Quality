7791b11cad0f15ea47d304476d0c4f87f95441eb
sean Goral <seangoral@ucsb.edu>
2018-03-01 17:46:04 -0800

    I did a lot of deleting and changing. Just did 3 tabs for the 3 bacteria parameters, got rid of chem tabs, and added h_lines for bacteria water quality stadards, but didn't annotate them or look up what the values are for either bacteria... If you could figure out a map and getting the rainfall dat on those graphs then that'd be epic. I'll mess with stuff sme over the weekend. Peace

--- water_quality/app.R
+++ water_quality/app.R
@@ -16,17 +16,47 @@ gol_bac <- read_csv("~/github/SB-Water-Quality/gol_bac.csv")
 gol_b<-gol_bac %>% 
   select(StationID,SampleDate,TestMaterial,ParameterCode,Result) 
 
-colnames(gol_b)<-c("stationid","date","testmaterial","parametercode","result")
+colnames(gol_b)<-c("station","date","testmaterial","parametercode","result")
+
+###################################################
+#Better bacteria datasets
+
+Goleta_bac_county <- read_csv("~/github/SB-Water-Quality/Goleta_bac_county.csv")
+
+Hope_Ranch_bac <- read_csv("~/github/SB-Water-Quality/Hope_Ranch_bac.csv")
+
+Sands_bac <- read_csv("~/github/SB-Water-Quality/Sands_bac.csv")
+
+Arroyo_Burro_bac <- read_csv("~/github/SB-Water-Quality/Arroyo_Burro_bac.csv")
+
+beach_bac<-rbind(Goleta_bac_county,Hope_Ranch_bac,Sands_bac,Arroyo_Burro_bac) %>% 
+  select("Station Name","Description","SampleDate","parameter","Result","unit")
+
+#reset column names, so they match what I called them below in the UI and server
+
+colnames(beach_bac)<-c("stationid","beach","date","parametercode","result","unit")
+
+beach_ecoli<-beach_bac %>% 
+  filter(parametercode=="E. Coli")
+
+beach_entero<-beach_bac %>% 
+  filter(parametercode=="Enterococcus")
+
+beach_total<-beach_bac %>% 
+  filter(parametercode=="Total Coliforms")
 
 #Goleta chemical dataset
+
 goleta_chem_sbck <- read_csv("~/github/SB-Water-Quality/goleta_chem_sbck.csv")
 View(goleta_chem_sbck)
 
 gol_c<-goleta_chem_sbck %>% 
   select(StationID,SampleDate,TestMaterial,ParameterCode,Result) 
 
-colnames(gol_c)<-c("stationid","date","testmaterial","parametercode","result")
+colnames(gol_c)<-c("station","date","testmaterial","parametercode","result")
 
+
+#########Chemical df's
 
 gol_cond<-gol_c %>% 
   filter(parametercode=="Conductivity")
@@ -73,16 +103,16 @@ rain$date <- as.Date(with(rain, paste(year, month, day,sep="-")), "%Y-%m-%d")
 
 ui <- dashboardPage(
   
-  dashboardHeader(title="Water Quality App"),
+  dashboardHeader(title="Beach Bacteria Concentrations"),
   
   dashboardSidebar(
     
     sidebarMenu(
       
-      menuItem("Bacteria",tabName = "tab_1"),
-      menuItem("Chemical Parameters",tabName = "tab_2"),
-      menuItem("Conductivity",tabName = "tab_3"),
-      menuItem("Temperature",tabName = "tab_4")
+      menuItem("E. Coli",tabName = "tab_1"),
+      menuItem("Enterococcus",tabName = "tab_2"),
+      menuItem("Total Coliforms",tabName = "tab_3")
+      
       
       
     )
@@ -94,9 +124,8 @@ ui <- dashboardPage(
       tabItem(tabName = "tab_1",
               fluidRow(
                 box(plotOutput("my_graph1",height = 700,width = 700)),
-                box(title = "choose FIB:",
-                    selectInput("parametercode","Parameter Code:",choices = unique(gol_b$parametercode)),
-                    selectInput("Stationid","Station ID:",choices = unique(gol_b$stationid)),
+                box(title = "E. Coli:",
+                    selectInput("stationid","Beach:",choices = unique(beach_bac$stationid)),
                     dateRangeInput("date", "Date range:",
                                    start = "2002-01-01",
                                    end   = "2017-12-31")
@@ -104,39 +133,23 @@ ui <- dashboardPage(
               )),
       tabItem(tabName = "tab_2",
               fluidRow(
-                box(plotOutput("my_graph2",height = 700,width=700)),
-                box(title = "Chemical Parameter:",
-                    selectInput("chem","Parameter Code:",choices = unique(gol_c$parametercode),width = 300),
-                    
-                    selectInput("station","Station ID:",choices = unique(gol_c$stationid)),
-                    
-                    # Input: 
+                box(plotOutput("my_graph2",height = 700,width = 700)),
+                box(title = "Enterococcus:",
+                    selectInput("stationid_2","Beach        :",choices = unique(beach_bac$stationid)),
                     dateRangeInput("date2", "Date range:",
                                    start = "2002-01-01",
-                                   end   = "2017-12-31"))
+                                   end   = "2017-12-31")
+                )
               )),
       tabItem(tabName = "tab_3",
               fluidRow(
                 box(plotOutput("my_graph3",height = 700,width=700)),
-                box(title = "conductivity",
+                box(title = "Total Coliform:",
                     
-                    selectInput("stationId","Station ID:",choices = unique(gol_c$stationid)),
+                    selectInput("stationid_3","Beach:",choices = unique(beach_total$stationid)),
                     
                     # Input: 
                     dateRangeInput("date3", "Date range:",
-                                   start = "2002-01-01",
-                                   end   = "2017-12-31"))
-              )),
-      
-      tabItem(tabName = "tab_4",
-              fluidRow(
-                box(plotOutput("my_graph4",height = 700,width=700)),
-                box(title = "Temperature",
-                    
-                    selectInput("stationID","Station ID:",choices = unique(gol_temp$stationid)),
-                    
-                    # Input: 
-                    dateRangeInput("date4", "Date range:",
                                    start = "2002-01-01",
                                    end   = "2017-12-31"))
               ))
@@ -157,75 +170,64 @@ server <- function(input,output){
   
   output$my_graph1<-renderPlot({
     
-    x <- gol_b$result 
+    x <- beach_ecoli$result 
     p<-input$parametercode
     d<-input$date
-    id<-input$Stationid
+    id<-input$stationid
     
-    ggplot(subset(gol_b_c,parametercode==p ),aes(date,result,color=stationid,0.3))+
-      geom_point()+
+    ggplot(subset(beach_ecoli,stationid==id ),aes(date,result,color=stationid,0.3))+
+      geom_line()+
       theme_bw()+
-      ggtitle("Goleta Bacteria Data")+
+      ggtitle("Beach E. Coli levels")+
       theme(plot.title = element_text(hjust = 0.5))+
       xlab("Sample Date")+
-      ylab("Result unit...")+
-      coord_cartesian(ylim = c(0, 10000))+
-      xlim(d)
+      ylab("MPN/100 mL")+
+      xlim(d)+
+      geom_hline(yintercept = 1500)
     
     
   })
+  
   output$my_graph2<-renderPlot({
     
-    x <- gol_b_c$result 
-    y<-input$chem
+    x <- beach_entero$result 
+    p<-input$parametercode
     d<-input$date2
-    id<-input$stationid
+    id<-input$stationid_2
     
-    ggplot(subset(gol_b_c,parametercode==y),aes(date,result,color=stationid,0.3))+
-      geom_point()+
+    ggplot(subset(beach_entero,stationid==id ),aes(date,result,color=stationid,0.3))+
+      geom_line()+
       theme_bw()+
-      ggtitle("Goleta Chem Data")+
+      ggtitle("Beach Enterococcus Levels")+
       theme(plot.title = element_text(hjust = 0.5))+
       xlab("Sample Date")+
-      ylab("Result unit...")+
-      coord_cartesian(ylim = c(0, 10000))+
-      xlim(d)
+      ylab("MPN/100 mL")+
+      xlim(d)+
+      geom_hline(yintercept = 1500)  
+      
+    
     
   })
   
   output$my_graph3<-renderPlot({
     
-    
+    x <- beach_total$result 
+    p<-input$parametercode
     d<-input$date3
-    id<-input$stationId
+    id<-input$stationid_3
     
-    ggplot(subset(gol_cond,stationid==id),aes(date,result,0.4))+
-      geom_point(color="blue")+
+    ggplot(subset(beach_total,stationid==id ),aes(date,result,color=stationid,0.3))+
+      geom_line()+
       theme_bw()+
-      ggtitle("Goleta Chem Data")+
+      ggtitle("Goleta Bacteria Data")+
       theme(plot.title = element_text(hjust = 0.5))+
       xlab("Sample Date")+
-      ylab("uS")+xlim(d)+
-      xlim(d)+geom_smooth(method = "lm")
+      ylab("MPN/100 mL")+
+      xlim(d)
+    
     
   })
   
-  output$my_graph4<-renderPlot({
-    
-    
-    d<-input$date4
-    id<-input$stationID
-    
-    ggplot(subset(gol_temp,stationid==id),aes(date,result,0.4))+
-      geom_point()+
-      theme_bw()+
-      ggtitle("Goleta Temp Data")+
-      theme(plot.title = element_text(hjust = 0.5))+
-      xlab("Sample Date")+
-      ylab("Degrees Celcius")+
-      xlim(d)+geom_smooth()
-    
-  })
   
 } 
 
